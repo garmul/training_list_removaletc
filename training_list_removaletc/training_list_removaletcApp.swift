d458837b7334020471415bfa4dfa1939d4354b87
Gary Williamson <gsmullings@gmail.com>
2024-04-20 23:12:59 -0400

    Initial Commit

---
+++ training_list_removaletc/training_list_removaletcApp.swift
@@ -0,0 +1,17 @@
+//
+//  training_list_removaletcApp.swift
+//  training_list_removaletc
+//
+//  Created by gary on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct training_list_removaletcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
